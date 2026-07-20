6c55b6ccf22c711a50b862409a19608d196c6f62
Marcus <juandbeta@gmail.com>
2026-07-20 06:16:03 +0200

    hello world index added

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,11 @@ const router = require('express').Router();
 
 router.use('/', require('./swagger'));
 
+router.get('/', (req, res) => {
+    //#swagger.tags = ['Hello world'];
+    res.send('Hello, World!');
+});
+
 router.use('/product', require('./product'));
 router.use('/categories', require('./categories'));
 
